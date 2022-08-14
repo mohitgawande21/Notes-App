d2e552a62583717685947dc49ebff6f1e72338d4
Mohit Gawande <mohitgawande65@gmail.com>
2022-08-14 13:06:33 +0530

    this is universal router component which can be renders if routes not matched

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function PageNotFound() {
+
+    return (
+        <div className='d-flex  justify-content-center'>
+            <div>
+                <h1>404</h1>
+                <h1 className="wrong-para">Oh! Page not found!</h1>
+            </div>
+        </div>
+    )
+}
