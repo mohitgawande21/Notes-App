6cf703d1420ba9e5c442f5736966a852af644932
Mohit Gawande <mohitgawande65@gmail.com>
2022-08-14 12:30:39 +0530

    this App.js file contains Main Employee.js component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Employee from './components/Employee'
+
+export default function App() {
+  const HeaderComponent = {
+    // overflow: "hidden"
+  }
+  return (
+
+    <Employee style={HeaderComponent} />
+  )
+}
