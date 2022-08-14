ce751f2690e4185c27619ce894d1b15c9a28ec0a
Mohit Gawande <mohitgawande65@gmail.com>
2022-08-14 13:19:40 +0530

    This having style for Modal components

---
+++ src/components/OverlayStyle.js
@@ -0,0 +1,15 @@
+export const Overlay={
+    position: "fixed",
+    width: "100%",
+    height: '100%',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    zIndex: '1',
+    cursor: "pointer",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+}
